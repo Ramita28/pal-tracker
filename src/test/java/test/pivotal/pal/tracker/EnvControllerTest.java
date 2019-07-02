d63e02d6326fd4f694d554055b559a0da85c46af
Ramita <ramitha.rokkam@wipro.com>
2019-07-02 09:28:09 -0700

    Lab 2

--- src/test/java/test/pivotal/pal/tracker/EnvControllerTest.java
+++ src/test/java/test/pivotal/pal/tracker/EnvControllerTest.java
@@ -5,7 +5,7 @@ import org.junit.Test;
 import java.util.Map;
 import io.pivotal.pal.tracker.EnvController;
 
-import static org.assertj.core.api.Assertions.assertThat;
+
 
 public class EnvControllerTest {
     @Test
--- src/test/java/test/pivotal/pal/tracker/WelcomeControllerTest.java
+++ src/test/java/test/pivotal/pal/tracker/WelcomeControllerTest.java
@@ -3,7 +3,7 @@ package test.pivotal.pal.tracker;
 import io.pivotal.pal.tracker.WelcomeController;
 import org.junit.Test;
 
-import static org.assertj.core.api.Assertions.assertThat;
+
 
 public class WelcomeControllerTest {
 
